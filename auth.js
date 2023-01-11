ae3d14e90a2e3bcca5f04e2ed6c8dd82ca482971
Dimitar <dimitar.delov24@gmail.com>
2023-01-11 16:19:28 +0100

    auth logic

--- auth.js
+++ auth.js
@@ -2,13 +2,37 @@
     const accountCustomer = document.querySelector('.account-customer');
     const accountSeller = document.querySelector('.account-seller');
 
-    accountCustomer.addEventListener('click', (ev) => {
+    accountCustomer?.addEventListener('click', (ev) => {
         accountCustomer.classList.add('selected-account');
         accountSeller.classList.remove('selected-account');
+        document.querySelectorAll('.seller-field').forEach((f) => f.remove());
     });
 
-    accountSeller.addEventListener('click', (ev) => {
-        accountSeller.classList.add('selected-account');
-        accountCustomer.classList.remove('selected-account');
+    accountSeller?.addEventListener('click', (ev) => {
+        if (document.querySelectorAll('.seller-field').length === 0) {
+            accountSeller.classList.add('selected-account');
+            accountCustomer.classList.remove('selected-account');
+
+            const el1 = document.createElement('div');
+            el1.classList.add('form-field', 'seller-field')
+            const el2 = document.createElement('div');
+            el2.classList.add('form-field', 'seller-field')
+
+            el1.innerHTML =    `<label for="company">Company Name</label>
+                                <input
+                                    type="text"
+                                    id="company"
+                                    placeholder="Company Ltd"
+                                />`;
+
+            el2.innerHTML =    `<label for="phone">Phone number</label>
+                                <input
+                                    type="tel"
+                                    id="phone"
+                                    placeholder="0XXXXXXXXX"
+                                />`;
+
+            document.querySelector('div > input[id="username"]').parentNode.after(el1, el2);
+        }
     });
 })();
