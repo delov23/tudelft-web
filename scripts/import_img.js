e318a43be3642c717616362eb1e661562b6abfbc
Dimitar <dimitar.delov24@gmail.com>
2023-01-13 15:12:47 +0100

    Fixes to paths

--- scripts/import_img.js
+++ scripts/import_img.js
@@ -1,7 +1,14 @@
-var loadFile = function (event) {
-  var output = document.getElementById("item-img");
-  output.src = URL.createObjectURL(event.target.files[0]);
-  output.onload = function () {
-    URL.revokeObjectURL(output.src); // free memory
+(function () {
+  const input = document.getElementById("item-images-field");
+  input.addEventListener("change", loadFile);
+
+  function loadFile(event) {
+    const output = document.getElementById("item-img");
+    output.src = URL.createObjectURL(event.target.files[0]);
+    output.onload = function () {
+      URL.revokeObjectURL(output.src);
+    };
+    const promptEl = document.querySelector(".img-container>p");
+    promptEl.style.display = "none";
   };
-};
+})();
