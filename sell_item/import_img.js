b7b1297f86e11fe617db1fe735579c2e238d1112
Aleks Yotkov <alexyotkov@gmail.com>
2023-01-15 20:51:34 +0100

    bug fixes

--- sell_item/import_img.js
+++ sell_item/import_img.js
@@ -1,14 +1,10 @@
 (function () {
   const input = document.getElementById("item-images-field");
-  input.addEventListener("change", loadFile);
-
-  function loadFile(event) {
+  input.addEventListener("change", () => {
     const output = document.querySelector(".item-img");
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function () {
-      URL.revokeObjectURL(output.src);
-    };
+    output.src = URL.createObjectURL(input.files[0]);
+
     const promptEl = document.querySelector(".img-container>p");
     promptEl.style.display = "none";
-  }
+  });
 })();
