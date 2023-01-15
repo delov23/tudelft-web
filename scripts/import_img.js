b577507c3a4d0acc590fddfd4738cb5f2bfe5bff
Aleks Yotkov <alexyotkov@gmail.com>
2023-01-15 18:15:07 +0100

    minor change to js script

--- scripts/import_img.js
+++ scripts/import_img.js
@@ -3,12 +3,12 @@
   input.addEventListener("change", loadFile);
 
   function loadFile(event) {
-    const output = document.getElementById("item-img");
+    const output = document.querySelector(".item-img");
     output.src = URL.createObjectURL(event.target.files[0]);
     output.onload = function () {
       URL.revokeObjectURL(output.src);
     };
     const promptEl = document.querySelector(".img-container>p");
     promptEl.style.display = "none";
-  };
+  }
 })();
