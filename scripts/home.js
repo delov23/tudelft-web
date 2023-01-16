d12ca641ece6e43d489f287615c0aaaaa7a6ed0e
Aleks Yotkov <alexyotkov@gmail.com>
2023-01-16 15:46:49 +0100

    minor fix

--- scripts/home.js
+++ scripts/home.js
@@ -6,15 +6,6 @@
       const toRemove = toAdd === "open" ? "closed" : "open";
       f.classList.add(toAdd);
       f.classList.remove(toRemove);
-
-      // f.style.display =
-      //     f.style.display === 'none' || f.style.display === ''
-      //         ? 'flex'
-      //         : 'none';
-      // const toAdd = f.classList.contains('op-1') ? 'op-0' : 'op-1';
-      // const toRemove = toAdd === 'op-0' ? 'op-1' : 'op-0';
-      // f.classList.add(toAdd);
-      // f.classList.remove(toRemove);
     });
     filterBtn.classList.toggle("upside-down");
   });
